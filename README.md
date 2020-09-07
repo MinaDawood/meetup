7c59e707326ec9267b5fd51a74b44676785c3a60
Mina <MinaDawood>
2020-09-07 07:07:33 +0200

    Push to Github

---
+++ README.md
@@ -0,0 +1,5 @@
+## Full stack project based upon a linkedin course
+
+# including tests
+
+# Performance and Security has been improved
--- server/app.js
+++ server/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 const path = require('path');
+const compression = require('compression');
 const createError = require('http-errors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const auth = require('./lib/auth');
 const SpeakerService = require('./services/SpeakerService');
 const FeedbackService = require('./services/FeedbackService');
+const AvatarService = require('./services/AvatarService');
 
-module.exports = (config) => {
+module.exports = config => {
   const app = express();
+  app.use(helmet());
+  app.use(compression());
   const speakers = new SpeakerService(config.data.speakers);
   const feedback = new FeedbackService(config.data.feedback);
+  const avatars = new AvatarService(config.data.avatars);
 
   app.set('view engine', 'pug');
   app.set('views', path.join(__dirname, './views'));
@@ -20,6 +31,20 @@ module.exports = (config) => {
   app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  app.use(
+    session({
+      secret: 'very secret 1234',
+      resave: true,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  );
+
+  app.use(auth.initialize);
+  app.use(auth.session);
+  app.use(auth.setUser);
 
   app.use(async (req, res, next) => {
     try {
@@ -31,7 +56,7 @@ module.exports = (config) => {
     }
   });
 
-  app.use('/', routes({ speakers, feedback }));
+  app.use('/', routes({ speakers, feedback, avatars }));
 
   // catch 404 and forward to error handler
   app.use((req, res, next) => {
